ae7c519fdd939855021961e7ab49352876713100
Javid izadi <javid.izadi1384@gmail.com>
2022-02-02 11:34:14 +0330

    add Title Component

---
+++ src/components/Title/Title.jsx
@@ -0,0 +1,35 @@
+const Title = () => {
+  return (
+    <div>
+      <div className="row">
+        <div className="col-lg-8 col-md-7 text-center text-md-start">
+          <h2
+            className="h1 mt-2 aos-init aos-animate"
+            data-aos="fade-left"
+            data-aos-delay="0"
+          >
+            Javid izadi
+          </h2>
+          <p
+            data-aos="fade-left"
+            data-aos-delay="100"
+            className="aos-init aos-animate"
+          >
+            .Net Developer
+          </p>
+          <div
+            className="d-print-none aos-init aos-animate"
+            data-aos="fade-left"
+            data-aos-delay="200"
+          >
+            <a className="btn btn-success shadow-sm mt-1" href="#contact">
+              Contact Me
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Title;
