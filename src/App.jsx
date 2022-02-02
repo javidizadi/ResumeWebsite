6b52f61f7b72b62ab28c2955ae91094959073b39
Javid izadi <javid.izadi1384@gmail.com>
2022-02-02 11:35:18 +0330

    making Main Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import Title from "./components/Title/Title";
 
 function App() {
   return (
-    <div>
+    <div className="container">
+      <div className="cover shadow-lg bg-white">
+        <div className="cover-bg p-3 p-lg-4 text-white">
+          <Title />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
